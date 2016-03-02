2966158724b63072959a953b2c8f161e8b359d91
vox <meisvox@icloud.com>
2016-03-01 16:11:55 -0800

    added view controllers to storyboard

---
+++ MakeMe/ReminderListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReminderListTableViewCell.swift
+//  MakeMe
+//
+//  Created by Vox on 3/1/16.
+//  Copyright © 2016 Yashley. All rights reserved.
+//
+
+import UIKit
+
+class ReminderListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
